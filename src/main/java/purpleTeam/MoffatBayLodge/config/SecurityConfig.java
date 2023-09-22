83f8d634ea2ce0aa8059c5177e072fddfa915d7d
RogueStrix <111405868+RogueStrix@users.noreply.github.com>
2023-09-22 18:55:41 -0500

    Added Attractions Page
    -- Added Attractions.jsp
    --Added 4 images for Attractions
    --Added Attractions Controller
    --Added Attractions Page CSS
    --Got rid of unused imports

--- src/main/java/purpleTeam/MoffatBayLodge/config/SecurityConfig.java
+++ src/main/java/purpleTeam/MoffatBayLodge/config/SecurityConfig.java
@@ -6,18 +6,12 @@ package purpleTeam.MoffatBayLodge.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
-
 import jakarta.servlet.DispatcherType;
 import purpleTeam.MoffatBayLodge.service.LodgeUserDetails;
 
